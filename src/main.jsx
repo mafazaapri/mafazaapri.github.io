3ce9ddd0d29742d9a7b77954bfbae65390472d69
Ihsan Nurul Iman <ihsannurul@hotmail.com>
2024-03-11 21:46:20 +0700

    Revert "adding 404 page"
    This reverts commit d7070ba40a8ac0ff88f4c96d95707da599121ec4.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createHashRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import WorkNorrisPage from "./pages/WorkNorrisPage.jsx";
@@ -8,8 +12,6 @@ import WorkPlatformePage from "./pages/WorkPlatformePage.jsx";
 import WorkRojorganicsPage from "./pages/WorkRojorganicsPage.jsx";
 import WorkHomeSpace from "./pages/WorkHomeSpace.jsx";
 import WorkBigGameGolf from "./pages/WorkBigGameGolf.jsx";
-import WorkBigGameGolfBranding from "./pages/WorkBigGameGolfBranding.jsx";
-import NotFoundPage from "./404.jsx";
 
 const router = createHashRouter([
   {
@@ -35,14 +37,6 @@ const router = createHashRouter([
   {
     path: "/work/big-game-golf",
     element: <WorkBigGameGolf />,
-  },
-  {
-    path: "/work/big-game-golf-branding",
-    element: <WorkBigGameGolfBranding />,
-  },
-  {
-    path: "*",
-    element: <NotFoundPage />,
   },
 ]);
 
