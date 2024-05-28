c733b0558eab55456d1e3e309214e2d3195b07f9
mafazaapri <161936619+mafazaapri@users.noreply.github.com>
2024-05-27 17:05:30 -0700

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,1 +1,12 @@
+import React from "react";
+import PageMetadata from "../components/PageMetadata";
+import HeaderImage from "../components/HeaderImage";
+import HeaderTitle from "../components/HeaderTitle";
+import Seperator from "../components/Seperator";
+import ReadNext from "../components/ReadNext";
+import BggPoint from "./page_components/bgg/BggPoint";
+import Footer from "../components/Footer";
 
+export default function WorkBigGameGolf() {
+  return (
+    <>
