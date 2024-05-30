c62a50cad8bf36c9abf12511f6e428dee2248706
mafazaapri <161936619+mafazaapri@users.noreply.github.com>
2024-05-30 11:10:52 -0700

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -22,7 +22,7 @@ const Homepage = () => {
           <div className="text-[2.5rem] lg:text-[6.25rem] text-start leading-none lg-leading-[6rem] mr mt-16 lg:mt-40">
             <span className="font-medium">
               I create
-              <br />
+      
               visuals that
             </span>
             <br />
