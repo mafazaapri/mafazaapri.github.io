7c951b6bd070d586f8ec72396a8771e209958783
mafazaapri <161936619+mafazaapri@users.noreply.github.com>
2024-05-30 10:11:55 -0700

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,8 +5,16 @@ import PageMetadata from "../components/PageMetadata";
 export default function AboutPage() {
   return (
     <>
-      <PageMetadata title="About" desc="About me" />
-      <div>About.</div>
-    </>
+
+
+     <PageMetadata
+        title="Big Game Golf"
+        desc="Redesigning an experience of connecting and searching legal service"
+      />
+      <HeaderImage
+        imgCoverName="bg-cover-big-game-golf.png"
+        imgName="bgg-page.png"
+        className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      />
   );
 }
