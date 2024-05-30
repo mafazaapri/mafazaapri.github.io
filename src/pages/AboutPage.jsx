7b53ed954c44916321b751b822776cbd5fca276f
mafazaapri <161936619+mafazaapri@users.noreply.github.com>
2024-05-30 10:38:21 -0700

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -10,6 +10,11 @@ export default function AboutPage() {
         title="Big Game Golf"
         desc="Redesigning an experience of connecting and searching legal service"
       />
+    <HeaderImage
+        imgCoverName="bg-cover-big-game-golf.png"
+        imgName="bgg-page.png"
+        className="top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      />
   </>
   );
 }
