611de191f045f050ed96d49d0d6161ce458db7c8
ihsansfd <ihsannurul@hotmail.com>
2024-05-30 20:22:10 +0700

    invalid class should be classname

--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -3,9 +3,9 @@ import React from "react";
 export default function NavItem({ title, url }) {
   return (
     <div className="mb-3 w-fit">
-      <a href={`${url}`} class="group transition duration-300">
+      <a href={`${url}`} className="group transition duration-300">
         {title}
-        <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-white"></span>
+        <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-white"></span>
       </a>
     </div>
   );
