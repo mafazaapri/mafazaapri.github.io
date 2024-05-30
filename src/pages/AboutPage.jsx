26b0ed9ec48dc88600a8302bea5412783fb0e383
mafazaapri <161936619+mafazaapri@users.noreply.github.com>
2024-05-30 10:55:01 -0700

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -15,6 +15,33 @@ export default function AboutPage() {
         imgName="homespace-homepage.png"
         className="lg:object-[50%_20%]"
       />
+    
+       <div className="container">
+        <section className="py-10">
+          <div className="text-[2.5rem] lg:text-[6.25rem] text-start leading-none lg-leading-[6rem] mr mt-16 lg:mt-40">
+            <span className="font-medium">
+              I create
+              <br />
+              visuals that
+            </span>
+            <br />
+            <span className="font-neuton italic">make sense</span>
+            <img src="/img/sprinkles.png" className="w-8 inline-block ms-2" />
+          </div>
+          <div className="flex flex-col lg:flex-row justify-between mt-10 lg:mt-16">
+            <div className="text-start mb-5 lg:mb-0 lg:max-w-[28rem]">
+              Hi I’m Mafaza{" "}
+              <img src="/img/hand.png" className="w-6 inline-block" /> , with my
+              experience I specialize in crafting seamless digital experiences
+              for{" "}
+              <span className="font-neuton italic">
+                websites, apps, & brands.
+              </span>
+              <br />I believe that every product is unique, and so is the design
+              process.
+            </div>
+          </div>
+        </section>
   </>
   );
 }
