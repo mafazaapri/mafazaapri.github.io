6118e75cbecf30b0def2e8f99c9bdc1cd3a94077
mafazaapri <161936619+mafazaapri@users.noreply.github.com>
2024-06-01 12:06:26 -0700

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,6 +7,19 @@ export default function AboutPage() {
     <>
       <PageMetadata title="About" desc="About me" />
       <div>About.</div>
+       <div className="flex flex-col lg:flex-row justify-between mt-10 lg:mt-16">
+            <div className="text-start mb-5 lg:mb-0 lg:max-w-[28rem]">
+              Hi I’m Mafaza{" "}
+              <img src="/img/hand.png" className="w-6 inline-block" /> , with my
+              experience I specialize in crafting seamless digital experiences
+              for{" "}
+              <span className="font-neuton italic">
+                websites, apps, & brands.
+              </span>
+              <br />I believe that every product is unique, and so is the design
+              process.
+            </div>
+          </div>
     </>
   );
 }
