cc40d8b015a1266909a2d2e3f993d5627889a65f
mafazaapri <161936619+mafazaapri@users.noreply.github.com>
2024-06-01 12:19:38 -0700

    Update AboutPage.jsx

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -10,11 +10,15 @@ export default function AboutPage() {
          <div className="container">
         <section className="py-10">
        <div className="flex flex-col lg:flex-row justify-between mt-10 lg:mt-16">
-            <div className="text-start mb-5 lg:mb-0 lg:max-w-[28rem]">
-              Hi I’m Mafaza{" "}
-              <img src="/img/hand.png" className="w-6 inline-block" /> , with my
+            
+                  <div className="text-[2.5rem] lg:text-[4.25rem] text-start leading-none lg-leading-[4rem] mr mt-16 lg:mt-40">
+            <span className="font-medium">
+            Hello{" "}
+              <img src="/img/hand.png" className="w-6 inline-block" /> , 
+              <br />with my
               experience I specialize in crafting seamless digital experiences
               for{" "}
+              </span>
               <span className="font-neuton italic">
                 websites, apps, & brands.
               </span>
